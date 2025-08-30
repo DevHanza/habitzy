937e8115bb066f2170454100d9deae294205c709
DevHanza <hansana123dasanayake@gmail.com>
2025-08-30 16:55:12 +0530

    add server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,8 @@
-//
+import app from "./index";
+import { connectDB } from "./config/db";
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
