8b7417f684a5f15ac98d253acdd6e8203f4b65f5
DevHanza <hansana123dasanayake@gmail.com>
2025-08-30 16:55:05 +0530

    add index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,19 @@
-//
+import express from "express";
+import cors from "cors";
+import habitRoutes from "./routes/habitRoutes";
+
+const app = express();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Load routes
+app.use("/api/habits", habitRoutes);
+
+export default app;
