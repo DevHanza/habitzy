43e627dfd8ed6b3fa8dc04050d6cc9877c091891
DevHanza <hansana123dasanayake@gmail.com>
2025-08-30 20:57:15 +0530

    Remove Unwanted Mongoose Properties

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,10 +8,7 @@ const MONGO_URL = process.env.MONGO_URI + process.env.DB_NAME;
 export const connectDB = async () => {
   //
   try {
-    await mongoose.connect(MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGO_URL, {});
     console.log(`Connected to ${process.env.DB_NAME} Database!`);
     //
   } catch (error) {
