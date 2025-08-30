907e2f684bf791ad218820648af86457975f90db
DevHanza <hansana123dasanayake@gmail.com>
2025-08-30 12:32:05 +0530

    Add Database Connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+let db;
+export async function connectDB() {
+  if (!db) {
+    await client.connect();
+    db = client.db("habitTracker");
+    console.log("Connected to the Database!");
+  }
+  return db;
+}
