7bac0e87c5a9c62fdf479ea5e254443f0227f87f
DevHanza <hansana123dasanayake@gmail.com>
2025-08-31 21:36:17 +0530

    Handle DB connection errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,10 @@ import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-  app.listen(PORT, () =>
-    console.log(`\nServer is running on port http://localhost:${PORT}/api/v1`)
-  );
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`\nServer is running on port http://localhost:${PORT}/api/v1`)
+    );
+  })
+  .catch((err) => console.error("DB connection error:", err));
