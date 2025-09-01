0078a6d725f8736d8652253eb7f3c5744146fa53
DevHanza <hansana123dasanayake@gmail.com>
2025-09-01 21:26:18 +0530

    Add a route for getHabitsByID

--- backend/src/controllers/habitController.js
+++ backend/src/controllers/habitController.js
@@ -1,9 +1,8 @@
 import { Habit } from "../models/habitModel.js";
 
 export async function getHabits(req, res) {
-  const { userId } = req.params;
-
   try {
+    const { userId } = req.params;
     const habits = await Habit.find({ userId: userId });
 
     if (!habits) {
@@ -17,10 +16,24 @@ export async function getHabits(req, res) {
   }
 }
 
-  console.log(id);
-  res.json({ id });
+//
+export async function getHabitByID(req, res) {
+  try {
+    const { userId, habitId } = req.params;
+    const habit = await Habit.find({
+      _id: habitId,
+      userId: userId,
+    });
+
+    if (!habit) {
+      return res.status(404).json({ message: "No habit found." });
+    }
+
+    res.json(habit);
+  } catch (errr) {
+    res.status(500).json({ message: err.message });
+  }
 }
-export async function getHabitByID(req, res) {}
-export async function addHabit(req, res) {}
+
 export async function deleteHabit(req, res) {}
 export async function updateHabit(req, res) {}
--- backend/src/routes/habitRoutes.js
+++ backend/src/routes/habitRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 
-import { getHabits } from "../controllers/habitController.js";
+import {
+  getHabits,
+  getHabitByID,
+  addHabit,
+} from "../controllers/habitController.js";
 
 // Enable "mergeParams" so we can access ":id" from parent route
 const router = Router({ mergeParams: true });
 
 router.get("/", getHabits);
+router.get("/:habitId", getHabitByID);
 
 export default router;
