74f0b6c584a16c0f5d1aed5d58efda9305e3de30
DevHanza <hansana123dasanayake@gmail.com>
2025-09-02 21:58:58 +0530

    Add a API Route: getDailyLogsByUser

--- backend/src/controllers/dailyLogController.js
+++ backend/src/controllers/dailyLogController.js
@@ -1,5 +1,20 @@
 import { DailyLog } from "../models/dailyLogModel.js";
 
-export async function getDailyLogsByUser(req, res) {}
+export async function getDailyLogsByUser(req, res) {
+  try {
+    const { userId } = req.params;
+    const dailyLogs = await DailyLog.find({
+      userId,
+    });
+
+    if (!dailyLogs) {
+      return res.status(404).json({
+        message: "No Daily Logs found.",
+      });
+    }
+
+    res.json(dailyLogs);
+  } catch (err) {}
+}
 export async function addDailyLog(req, res) {}
 export async function updateDailyLog(req, res) {}
--- backend/src/routes/dailyLogRoutes.js
+++ backend/src/routes/dailyLogRoutes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
+import { getDailyLogsByUser } from "../controllers/dailyLogController.js";
 
 // Enable "mergeParams" so we can access ":id" from parent route
 const router = Router({ mergeParams: true });
+
+router.get("/", getDailyLogsByUser);
 
 export default router;
