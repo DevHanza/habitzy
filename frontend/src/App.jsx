c43ffc247f5dda03dae3c74beec8d528f230e2ce
DevHanza <hansana123dasanayake@gmail.com>
2025-09-06 18:55:52 +0530

    Install & Setup Chakra UI with Basic Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,7 @@
 import { Button, HStack } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default App;
