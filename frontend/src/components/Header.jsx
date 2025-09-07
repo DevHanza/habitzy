09c253eb9b3c65d390f40f272a42848f3652abc8
DevHanza <hansana123dasanayake@gmail.com>
2025-09-07 17:40:43 +0530

    Refactor Header Code & Use <For> Component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,19 @@
-import { IconButton, useBreakpointValue } from "@chakra-ui/react";
-
-import { Flex } from "@chakra-ui/react/flex";
-import { Box } from "@chakra-ui/react/box";
-import { Container } from "@chakra-ui/react/container";
-import { Text } from "@chakra-ui/react/text";
-import { Stack } from "@chakra-ui/react/stack";
-
-import { Link } from "react-router";
-
 import ProfileIcon from "./ProfileIcon";
 import HabitsTrackerLogo from "./Logo";
+
+import {
+  Flex,
+  Box,
+  Container,
+  Text,
+  Stack,
+  For,
+  IconButton,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
 import { Menu } from "lucide-react";
+import { Link } from "react-router";
 
 const menuItems = [
   {
@@ -52,15 +55,15 @@ function DesktopMenu() {
   return (
     <Stack direction={"row"} gap={"10"} alignItems={"center"}>
       <Stack gap={"2rem"} direction={"row"}>
-        {menuItems.map((item) => {
-          return (
+        <For each={menuItems}>
+          {(item) => (
             <Link key={item.link} to={item.link}>
               <Text color={"gray.400"} _hover={{ color: "gray.50" }}>
                 {item.label}
               </Text>
             </Link>
-          );
-        })}
+          )}
+        </For>
       </Stack>
       <ProfileIcon />
     </Stack>
