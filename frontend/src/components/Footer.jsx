8b56006faf1df686838970217b305cd0672bfe80
DevHanza <hansana123dasanayake@gmail.com>
2025-09-08 20:53:49 +0530

    Add Footer Code

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,18 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Stack, Box, Text } from "@chakra-ui/react";
 
 function Footer() {
   return (
     <footer>
-      <Container>Footer</Container>
+      <Box py={"0.5rem"}>
+        <Container>
+          <Stack>
+            <Text textAlign={"center"} fontSize={"0.75rem"} color={"fg.muted"}>
+              Thanks for using Habit Tracker. ©{" "}
+              <a href="https://devhanza.is-a.dev">DevHanza</a>
+            </Text>
+          </Stack>
+        </Container>
+      </Box>
     </footer>
   );
 }
