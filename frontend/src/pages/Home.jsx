ec949662f2eebd325f72fc6191ef16f40ff3202a
DevHanza <hansana123dasanayake@gmail.com>
2025-09-09 07:57:08 +0530

    Add Home Layout for Dashboard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,16 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Flex, Stack } from "@chakra-ui/react";
 
 function Home() {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <Flex gap={4}>
+        <Stack flexGrow={1}></Stack>
+
+        <Stack flexGrow={4}></Stack>
+        <Stack flexGrow={1}></Stack>
+      </Flex>
+    </Container>
+  );
 }
 
 export default Home;
