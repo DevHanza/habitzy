ad1c32e7b0295ebe5f692e000a1ebdb962d5161e
DevHanza <hansana123dasanayake@gmail.com>
2025-09-11 21:51:16 +0530

    Add Final Styles to ProgressBox Component

--- frontend/src/components/ProgressBox.jsx
+++ frontend/src/components/ProgressBox.jsx
@@ -1,18 +1,38 @@
 import WidgetsWrapper from "./ui/WidgetsWrapper";
-import { Stack, HStack, Progress, Heading } from "@chakra-ui/react";
+import { Stack, HStack, Progress, Heading, Text } from "@chakra-ui/react";
 
 function ProgressBox() {
   return (
     <WidgetsWrapper>
       <Stack>
-        <Progress.Root defaultValue={40}>
-          <Heading fontSize={"1.5rem"}>40%</Heading>
-          <HStack gap="5">
+        <Stack mb={"1rem"} direction={"row"} alignItems={"end"} gap={0}>
+          <Heading
+            display={"inline-block"}
+            size={{ base: "2xl", md: "4xl" }}
+            fontWeight={700}
+          >
+            40%
+          </Heading>
+          <Heading
+            color={"fg.muted"}
+            display={"inline-block"}
+            size={{ base: "md", md: "lg" }}
+            fontWeight={500}
+          >
+            &nbsp; out of 100%.
+          </Heading>
+        </Stack>
+
+        <Progress.Root defaultValue={40} colorPalette={""}>
+          <HStack>
             <Progress.Track flex="1">
               <Progress.Range />
             </Progress.Track>
           </HStack>
         </Progress.Root>
+        <Text marginTop={".5em"} color={"fg.muted"}>
+          Complete your daily goals to win both of your day and streak.
+        </Text>
       </Stack>
     </WidgetsWrapper>
   );
