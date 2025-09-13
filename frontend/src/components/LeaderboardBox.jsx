b123832f4c5cb785d9e129eeb971f6ddc2e22c13
DevHanza <hansana123dasanayake@gmail.com>
2025-09-13 10:44:01 +0530

    Add Leaderboard Card & Changes to LeaderboardBox

--- frontend/src/components/LeaderboardBox.jsx
+++ frontend/src/components/LeaderboardBox.jsx
@@ -1,20 +1,41 @@
-import { Heading, HStack, Image } from "@chakra-ui/react";
+import {
+  Heading,
+  Stack,
+  VStack,
+  HStack,
+  Button,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import { ArrowRight } from "lucide-react";
 import WidgetsWrapper from "./ui/WidgetsWrapper";
+import LeaderboardCard from "./LeaderboardCard";
+import { Link } from "react-router";
 
 function LeaderboardBox() {
   return (
     <WidgetsWrapper>
-      <HStack
-        justifyContent={"space-between"}
-        marginBottom={{ base: "1rem", md: "1.5rem" }}
-      >
-        <Heading>Leaderboard</Heading>
-        <Image
-          src="https://emojicdn.elk.sh/👑?style=facebook"
-          height={{ base: "3.5rem", md: "2rem" }}
-          width={{ base: "3.5rem", md: "2rem" }}
-        ></Image>
-      </HStack>
+      <Stack gap={0}>
+        <HStack
+          justifyContent={"space-between"}
+          marginBottom={{ base: "1rem", md: "1.5rem" }}
+        >
+          <Heading>Leaderboard</Heading>
+          <ChakraLink
+            as={Link}
+            to={"/leaderboard"}
+            outlineColor={"transparent"}
+            fontSize={"0.75rem"}
+          >
+            See more <ArrowRight size={"1rem"} />
+          </ChakraLink>
+        </HStack>
+        <VStack gap={2}>
+          <LeaderboardCard />
+          <LeaderboardCard />
+          <LeaderboardCard />
+          <LeaderboardCard />
+        </VStack>
+      </Stack>
     </WidgetsWrapper>
   );
 }
---
+++ frontend/src/components/LeaderboardCard.jsx
@@ -0,0 +1,60 @@
+import { Box, Avatar, HStack, Text, VStack, Image } from "@chakra-ui/react";
+
+function LeaderboardCard() {
+  return (
+    <Box
+      borderRadius={6}
+      px={3}
+      py={2}
+      _hover={{
+        bg: "bg.emphasized",
+      }}
+      borderColor={"fg.subtle"}
+      width={"100%"}
+    >
+      <HStack gap={5}>
+        {/* Rank */}
+        <Text fontWeight={700} color={"fg.muted"}>
+          1
+        </Text>
+        {/* Rank */}
+        <HStack justifyContent={"space-between"} flex={3}>
+          {/* User */}
+          <HStack gap={3}>
+            <Avatar.Root>
+              <Avatar.Fallback name="Jhon Doe" />
+              <Avatar.Image src="https://bit.ly/sage-adebayo" />
+            </Avatar.Root>
+            <VStack gap={1} alignItems={"flex-start"}>
+              <Text fontSize={"md"} fontWeight={500} lineHeight={1}>
+                Hansana
+              </Text>
+              <Text
+                fontSize={"xs"}
+                fontWeight={300}
+                lineHeight={1}
+                color={"fg.muted"}
+              >
+                @devhanza
+              </Text>
+            </VStack>
+          </HStack>
+          {/* User */}
+
+          {/* Streak */}
+          <HStack gap={1.5}>
+            <Text fontWeight={500}>100</Text>
+            <Image
+              src="https://emojicdn.elk.sh/🔥?style=facebook"
+              height={{ base: "1rem", md: "1.15rem" }}
+              width={{ base: "1rem", md: "1.15rem" }}
+            ></Image>
+          </HStack>
+          {/* Streak */}
+        </HStack>
+      </HStack>
+    </Box>
+  );
+}
+
+export default LeaderboardCard;
