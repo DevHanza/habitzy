b972d96b2f3ced015fe151de42a63a211257aa92
DevHanza <hansana123dasanayake@gmail.com>
2025-09-13 13:31:19 +0530

    Refactor LeaderboardBox

--- frontend/src/components/LeaderboardBox.jsx
+++ frontend/src/components/LeaderboardBox.jsx
@@ -1,41 +1,16 @@
-import {
-  Heading,
-  Stack,
-  VStack,
-  HStack,
-  Button,
-  Link as ChakraLink,
-} from "@chakra-ui/react";
-import { ArrowRight } from "lucide-react";
-import WidgetsWrapper from "./ui/WidgetsWrapper";
+import { VStack } from "@chakra-ui/react";
+import WidgetsWrapper from "./ui/WidgetWrapper";
 import LeaderboardCard from "./LeaderboardCard";
-import { Link } from "react-router";
 
 function LeaderboardBox() {
   return (
     <WidgetsWrapper>
-      <Stack gap={0}>
-        <HStack
-          justifyContent={"space-between"}
-          marginBottom={{ base: "1rem", md: "1.5rem" }}
-        >
-          <Heading>Leaderboard</Heading>
-          <ChakraLink
-            as={Link}
-            to={"/leaderboard"}
-            outlineColor={"transparent"}
-            fontSize={"0.75rem"}
-          >
-            See more <ArrowRight size={"1rem"} />
-          </ChakraLink>
-        </HStack>
-        <VStack gap={2}>
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-        </VStack>
-      </Stack>
+      <VStack gap={2}>
+        <LeaderboardCard />
+        <LeaderboardCard />
+        <LeaderboardCard />
+        <LeaderboardCard />
+      </VStack>
     </WidgetsWrapper>
   );
 }
