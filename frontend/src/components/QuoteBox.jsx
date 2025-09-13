38922e854d8bb634654f7fa14bd4ece70a1a3fc8
DevHanza <hansana123dasanayake@gmail.com>
2025-09-13 13:14:57 +0530

    Redesign QuoteBox

--- frontend/src/components/QuoteBox.jsx
+++ frontend/src/components/QuoteBox.jsx
@@ -1,23 +1,14 @@
 import { Heading, Stack, Text } from "@chakra-ui/react";
-import WidgetsWrapper from "./ui/WidgetsWrapper";
+import WidgetsWrapper from "./ui/WidgetWrapper";
 
 function QuoteBox() {
   return (
     <WidgetsWrapper>
-      <Stack>
-        <Text
-          fontSize={"0.875rem"}
-          color={"fg.muted"}
-          textTransform={"uppercase"}
-          fontWeight={500}
-        >
-          Quote of the day
-        </Text>
+      <Stack gap={4}>
         <Heading
           fontWeight={500}
           size={{ base: "md", md: "lg" }}
           lineHeight={{ base: 1.25, md: 1.3 }}
-          marginBlock={{ base: "0.75rem", md: "1rem" }}
         >
           If you don't sacrifice for your goals, your goals become the
           sacrifice.
