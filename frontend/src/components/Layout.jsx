4ada3ab907560a99d8a31a59eacc235f3a9ad1f9
DevHanza <hansana123dasanayake@gmail.com>
2025-09-14 14:26:43 +0530

    Add a gap beween header, main & footer

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Stack } from "@chakra-ui/react";
 
 function Layout() {
   return (
-    <>
+    <Stack gap={8}>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </Stack>
   );
 }
 
