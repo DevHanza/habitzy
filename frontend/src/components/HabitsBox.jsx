4e268c5964f69c20d770d67b10c5b906508b1902
DevHanza <hansana123dasanayake@gmail.com>
2025-09-17 21:38:01 +0530

    Add demo data for habits

--- frontend/src/components/HabitsBox.jsx
+++ frontend/src/components/HabitsBox.jsx
@@ -1,7 +1,20 @@
 import WidgetsWrapper from "./ui/WidgetWrapper";
 import HabitCard from "./HabitCard";
 import { Plus } from "lucide-react";
-import { Button, Stack, VStack } from "@chakra-ui/react";
+import { Button, For, Stack, VStack } from "@chakra-ui/react";
+
+const habits = [
+  { icon: "📖", name: "Read a book" },
+  { icon: "🏃‍♂️", name: "Go for a run" },
+  { icon: "🧘‍♀️", name: "Meditate" },
+  { icon: "💧", name: "Drink water" },
+  { icon: "📝", name: "Journal" },
+  { icon: "🍎", name: "Eat healthy" },
+  { icon: "🛏️", name: "Sleep early" },
+  { icon: "🎸", name: "Practice guitar" },
+  { icon: "🌱", name: "Gardening" },
+  { icon: "🚿", name: "Cold shower" },
+];
 
 function HabitsBox() {
   return (
@@ -9,18 +22,16 @@ function HabitsBox() {
       // bg={"none"}
       // bg={"bg.subtle"}
       // border="none"
-      height={"100%"}
+      // height={"100%"}
       title={"Habits"}
       buttonText={"Add Habit"}
       buttonIcon={<Plus />}
     >
       <Stack gap={6}>
         <VStack gap={2}>
-          <HabitCard />
-          <HabitCard />
-          <HabitCard />
-          <HabitCard />
-          <HabitCard />
+          <For each={habits}>
+            {(habit) => <HabitCard habit={habit.name} icon={habit.icon} />}
+          </For>
         </VStack>
         <VStack>
           <Button variant="solid" colorPalette={"teal"} width={"100%"}>
