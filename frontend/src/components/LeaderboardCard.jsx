bbd41089ea7d4d01f3df5685293747399e7a7673
DevHanza <hansana123dasanayake@gmail.com>
2025-09-17 16:15:54 +0530

    Make the Leaderboard Card Reusable: Add Props

--- frontend/src/components/LeaderboardCard.jsx
+++ frontend/src/components/LeaderboardCard.jsx
@@ -1,7 +1,12 @@
 import { Box, Avatar, HStack, Text, VStack, Image } from "@chakra-ui/react";
 import pickPalette from "@/utils/pickPallette";
 
-function LeaderboardCard() {
+function LeaderboardCard({
+  rank = "#1",
+  name = "Hansana",
+  username = "@devhanza",
+  streak = 100,
+}) {
   return (
     <Box
       borderRadius={6}
@@ -16,19 +21,19 @@ function LeaderboardCard() {
       <HStack gap={4}>
         {/* Rank */}
         <Text fontWeight={600} color={"fg.muted"}>
-          #1
+          {rank}
         </Text>
         {/* Rank */}
         <HStack justifyContent={"space-between"} flex={3}>
           {/* User */}
           <HStack gap={3}>
             <Avatar.Root colorPalette={pickPalette(name)}>
-              <Avatar.Fallback name="Jhon Doe" />
-              <Avatar.Image src="https://bit.ly/sage-adebayo" />
+              <Avatar.Fallback name={name} />
+              {/* <Avatar.Image src="https://bit.ly/sage-adebayo" /> */}
             </Avatar.Root>
             <VStack gap={1} alignItems={"flex-start"}>
               <Text fontSize={"md"} fontWeight={500} lineHeight={1}>
-                Hansana
+                {name}
               </Text>
               <Text
                 fontSize={"xs"}
@@ -36,7 +41,7 @@ function LeaderboardCard() {
                 lineHeight={1}
                 color={"fg.muted"}
               >
-                @devhanza
+                {username}
               </Text>
             </VStack>
           </HStack>
@@ -44,7 +49,7 @@ function LeaderboardCard() {
 
           {/* Streak */}
           <HStack gap={1.5}>
-            <Text fontWeight={500}>100</Text>
+            <Text fontWeight={500}>{streak}</Text>
             <Image
               src="https://emojicdn.elk.sh/🔥?style=facebook"
               height={{ base: "1rem", md: "1.15rem" }}
