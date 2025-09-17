c98de57394176bc04834261b060b253b95d10322
DevHanza <hansana123dasanayake@gmail.com>
2025-09-17 15:43:16 +0530

    Add Nav Items (New) to ProfileBox Menu

--- frontend/src/components/ProfileBox.jsx
+++ frontend/src/components/ProfileBox.jsx
@@ -3,11 +3,23 @@ import {
   Text,
   Stack,
   HStack,
-  Box,
   Menu,
   Portal,
+  Switch,
 } from "@chakra-ui/react";
-import { ChevronDown, UserRound, Settings } from "lucide-react";
+import {
+  ChevronDown,
+  UserRound,
+  Settings,
+  LogOut,
+  MoonStarIcon,
+} from "lucide-react";
+import { Link } from "react-router";
+
+const profileBoxMenu = [
+  { label: "Your Profile", link: "#", icon: UserRound },
+  { label: "Settings", link: "#", icon: Settings },
+];
 
 function ProfileBox({ extended = false }) {
   const user = {
@@ -19,10 +31,11 @@ function ProfileBox({ extended = false }) {
   return (
     // Profile Box Menu - Start
 
-    <Menu.Root positioning={{ placement: "bottom" }}>
+    <Menu.Root positioning={{ placement: "top-start" }}>
       {/* Profile Box - Start */}
       <Menu.Trigger asChild cursor={"pointer"}>
         <HStack
+          cursor={"pointer"}
           w="100%"
           justifyContent={extended ? "space-between" : "flex-start"}
         >
@@ -49,15 +62,87 @@ function ProfileBox({ extended = false }) {
 
       <Portal>
         <Menu.Positioner>
-          <Menu.Content>
-            <Menu.Item value="profile">
-              <UserRound size={"1.125rem"} />
-              <Text fontSize={"md"}>My Profile</Text>
-            </Menu.Item>
-            <Menu.Item value="settings">
-              <Settings size={"1.125rem"} />
-              <Text fontSize={"md"}>Settings</Text>
-            </Menu.Item>
+          <Menu.Content maxWidth={"300px"} width={"100%"} p={3}>
+            {/* Profile  */}
+            <HStack
+              gap="4"
+              px={2}
+              py={2}
+              cursor={"pointer"}
+              borderRadius={4}
+              _hover={{ bg: "bg.emphasized/60" }}
+            >
+              <Avatar.Root>
+                <Avatar.Fallback name={"DevHanza"} />
+                <Avatar.Image />
+              </Avatar.Root>
+              <Stack gap="0">
+                <Text fontWeight="medium">Hansana</Text>
+                <Text
+                  color="fg.muted"
+                  textStyle="sm"
+                  width={"200px"}
+                  whiteSpace={"nowrap"}
+                  textOverflow={"ellipsis"}
+                  overflow={"hidden"}
+                >
+                  devhanza@domain.com
+                </Text>
+              </Stack>
+            </HStack>
+            <hr style={{ margin: "0.5em 0" }} />
+            {/* Profile  */}
+            {profileBoxMenu.map((item) => {
+              return (
+                <Menu.Item value={item.label} py={2.5} px={2} gap={4}>
+                  <Link to={item.link} style={{ outline: "transparent" }}>
+                    <Stack direction={"row"}>
+                      <item.icon
+                        size={"1.125rem"}
+                        style={{ color: "#a1a1aa" }}
+                      />
+                      <Text fontSize={"md"}>{item.label}</Text>
+                    </Stack>
+                  </Link>
+                </Menu.Item>
+              );
+            })}
+
+            <Stack
+              direction={"row"}
+              py={2.5}
+              px={2}
+              borderRadius={2}
+              justifyContent={"space-between"}
+              _hover={{ bg: "bg.emphasized/60" }}
+            >
+              <Stack direction={"row"} alignItems={"center"}>
+                <MoonStarIcon size={"1.125rem"} style={{ color: "#a1a1aa" }} />
+                Switch to Dark
+              </Stack>
+
+              <Switch.Root size={"md"}>
+                <Switch.HiddenInput />
+                <Switch.Control />
+                <Switch.Label />
+              </Switch.Root>
+            </Stack>
+
+            <hr style={{ margin: "0.5em 0" }} />
+
+            <Link to={"#"} style={{ outline: "transparent" }}>
+              <Stack
+                direction={"row"}
+                py={2.5}
+                px={2}
+                borderRadius={2}
+                alignItems={"center"}
+                _hover={{ bg: "bg.emphasized/60" }}
+              >
+                <LogOut size={"1.125rem"} style={{ color: "#a1a1aa" }} />
+                <Text fontSize={"md"}>Log Out</Text>
+              </Stack>
+            </Link>
           </Menu.Content>
         </Menu.Positioner>
       </Portal>
