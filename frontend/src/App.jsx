aabfa6b78eebaaac4b9a4a581cdeabc5bcb7238d
DevHanza <hansana123dasanayake@gmail.com>
2025-09-20 09:33:09 +0530

    Wrap the App.jsx with HabitProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import { RouterProvider } from "react-router";
 import router from "./routes";
+import { HabitProvider } from "./context/HabitProvider";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HabitProvider>
+      <RouterProvider router={router} />
+    </HabitProvider>
+  );
 }
 
 export default App;
