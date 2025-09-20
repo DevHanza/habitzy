af9d4b3fc206669e349a18cf00660506ba73c03b
DevHanza <hansana123dasanayake@gmail.com>
2025-09-20 13:57:10 +0530

    Update the state and adding a new habit to the list

--- frontend/src/components/AddHabitBox.jsx
+++ frontend/src/components/AddHabitBox.jsx
@@ -2,16 +2,30 @@ import { Box, Input, Stack, IconButton } from "@chakra-ui/react";
 import { Check, X } from "lucide-react";
 import EmojiPickerButton from "./EmojiPickerButton/EmojiPickerButton";
 import useHabits from "@/hooks/useHabits";
+import { useRef, useState, useEffect } from "react";
 
 function AddHabitBox() {
-  const { setIsAddingHabits } = useHabits();
+  const inputRef = useRef();
+  const [selectedEmoji, setSelectedEmoji] = useState(null);
+  const { addHabit, setIsAddingHabits } = useHabits();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   const handleAddHabit = () => {
-    setIsAddingHabits((prev) => !prev);
+    if (inputRef.current.value.trim().length === 0) return;
+
+    if (inputRef.current) {
+      addHabit({ icon: selectedEmoji || "✨", name: inputRef.current.value });
+      inputRef.current.value = "";
+      setIsAddingHabits((prev) => !prev);
+    }
   };
 
   const handleCancelAddHabit = () => {
     setIsAddingHabits((prev) => !prev);
+    inputRef.current.value = "";
   };
 
   return (
@@ -26,40 +40,53 @@ function AddHabitBox() {
       //   borderColor: "teal.500",
       // }}
     >
-      <Stack direction={"row"} alignItems={"center"} px={3}>
-        {/* Add Emoji Button */}
-        <EmojiPickerButton />
-        <Input
-          py={4}
-          px={0}
-          placeholder="Enter habit name"
-          size={"lg"}
-          fontSize={{ base: "1.125rem", md: "1.25rem" }}
-          fontWeight={500}
-          height={"auto"}
-          border={0}
-          outline={"transparent"}
-        />
-        {/* Done Button */}
-        <IconButton
-          size="sm"
-          colorPalette={"teal"}
-          aria-label="Add habit"
-          onClick={handleAddHabit}
-        >
-          <Check />
-        </IconButton>
-        {/* Cancel Button */}
-        <IconButton
-          size="sm"
-          variant={"surface"}
-          colorPalette={"teal"}
-          aria-label="Cancel Adding a habit"
-          onClick={handleCancelAddHabit}
-        >
-          <X />
-        </IconButton>
-      </Stack>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddHabit();
+        }}
+      >
+        <Stack direction={"row"} alignItems={"center"} px={3}>
+          {/* Add Emoji Button */}
+          <EmojiPickerButton
+            selectedEmoji={selectedEmoji}
+            setSelectedEmoji={setSelectedEmoji}
+          />
+          <Input
+            required
+            ref={inputRef}
+            py={4}
+            px={0}
+            placeholder="Enter habit name"
+            size={"lg"}
+            fontSize={{ base: "1.125rem", md: "1.25rem" }}
+            fontWeight={500}
+            height={"auto"}
+            border={0}
+            outline={"transparent"}
+          />
+          {/* Done Button */}
+          <IconButton
+            type="submit"
+            size="sm"
+            colorPalette={"teal"}
+            aria-label="Add habit"
+            // onClick={handleAddHabit}
+          >
+            <Check />
+          </IconButton>
+          {/* Cancel Button */}
+          <IconButton
+            size="sm"
+            variant={"surface"}
+            colorPalette={"teal"}
+            aria-label="Cancel Adding a habit"
+            onClick={handleCancelAddHabit}
+          >
+            <X />
+          </IconButton>
+        </Stack>
+      </form>
     </Box>
   );
 }
