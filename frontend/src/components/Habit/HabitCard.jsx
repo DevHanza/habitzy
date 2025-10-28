6e6f2dd2c22d6a7c4a0bf642e73d7711f39068a6
DevHanza <hansana123dasanayake@gmail.com>
2025-10-28 16:50:05 +0530

    Fix Unwanted rerenders in HabitsBox again : byRemove HabitControlMenu Wrapper from the Habit Card

--- frontend/src/components/Habit/HabitCard.jsx
+++ frontend/src/components/Habit/HabitCard.jsx
@@ -4,7 +4,6 @@ import {
   draggable,
   dropTargetForElements,
 } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
-import HabitControlMenu from "@/components/Habit/HabitControlMenu";
 import EditHabitBox from "@/components/Habit/EditHabitBox";
 
 const HabitCard = memo(function HabitCard({
@@ -78,67 +77,65 @@ const HabitCard = memo(function HabitCard({
           icon={icon}
         />
       ) : (
-        <HabitControlMenu id={id} setIsEditing={setIsEditing}>
-          <Box
-            borderRadius={6}
-            px={3}
-            py={4}
-            border={"0.5px solid"}
-            borderColor={"border"}
-            bg={"bg.subtle"}
-            width={"100%"}
-            position={"relative"}
-            _hover={{
-              bg: "bg.emphasized",
-              cursor: "pointer",
+        <Box
+          borderRadius={6}
+          px={3}
+          py={4}
+          border={"0.5px solid"}
+          borderColor={"border"}
+          bg={"bg.subtle"}
+          width={"100%"}
+          position={"relative"}
+          _hover={{
+            bg: "bg.emphasized",
+            cursor: "pointer",
+          }}
+          ref={ref}
+          opacity={isDragging ? 0.5 : 1}
+          outline={isDraggingOver ? "2px dashed" : 0}
+          outlineColor={"teal.500"}
+        >
+          <span
+            style={{ position: "absolute", inset: 0, zIndex: 5 }}
+            onClick={() => {
+              toggleHabit(id);
             }}
-            ref={ref}
-            opacity={isDragging ? 0.5 : 1}
-            outline={isDraggingOver ? "2px dashed" : 0}
-            outlineColor={"teal.500"}
+          ></span>
+
+          <Stack
+            flexDirection={"row"}
+            align="flex-start"
+            flex="1"
+            alignItems={"center"}
           >
-            <span
-              style={{ position: "absolute", inset: 0, zIndex: 5 }}
-              onClick={() => {
+            <Image
+              src={`https://emojicdn.elk.sh/${icon}?style=facebook`}
+              opacity={isCompleted ? 0.5 : 1}
+              height={{ base: "1rem", md: "1.5rem" }}
+              width={{ base: "1rem", md: "1.5rem" }}
+            />
+            <Checkbox.Root
+              variant={"solid"}
+              colorPalette={"teal"}
+              checked={isCompleted}
+              onCheckedChange={() => {
                 toggleHabit(id);
               }}
-            ></span>
-
-            <Stack
-              flexDirection={"row"}
-              align="flex-start"
-              flex="1"
-              alignItems={"center"}
+              width={"100%"}
+              justifyContent={"space-between"}
             >
-              <Image
-                src={`https://emojicdn.elk.sh/${icon}?style=facebook`}
-                opacity={isCompleted ? 0.5 : 1}
-                height={{ base: "1rem", md: "1.5rem" }}
-                width={{ base: "1rem", md: "1.5rem" }}
-              />
-              <Checkbox.Root
-                variant={"solid"}
-                colorPalette={"teal"}
-                checked={isCompleted}
-                onCheckedChange={() => {
-                  toggleHabit(id);
-                }}
-                width={"100%"}
-                justifyContent={"space-between"}
+              <Checkbox.Label
+                textDecoration={isCompleted ? "line-through" : ""}
+                fontSize={{ base: "1.125rem", md: "1.25rem" }}
+                color={isCompleted ? "fg.subtle" : ""}
               >
-                <Checkbox.Label
-                  textDecoration={isCompleted ? "line-through" : ""}
-                  fontSize={{ base: "1.125rem", md: "1.25rem" }}
-                  color={isCompleted ? "fg.subtle" : ""}
-                >
-                  {label}
-                </Checkbox.Label>
-                <Checkbox.HiddenInput />
-                <Checkbox.Control />
-              </Checkbox.Root>
-            </Stack>
-          </Box>
-        </HabitControlMenu>
+                {label}
+              </Checkbox.Label>
+              <Checkbox.HiddenInput />
+              <Checkbox.Control />
+            </Checkbox.Root>
+          </Stack>
+        </Box>
       )}
     </>
   );
