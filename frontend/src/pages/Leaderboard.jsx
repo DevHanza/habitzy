843d859bf92d79f47cb5988c9dd525c73846aad1
DevHanza <hansana123dasanayake@gmail.com>
2025-10-30 22:02:40 +0530

    Added Basic Structure for Settings Page

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -1,5 +1,23 @@
+import { Flex, Container, Stack, Heading, Text } from "@chakra-ui/react";
+
 function Leaderboard() {
-  return <div>Coming Soon!</div>;
+  return (
+    <Container maxW={"xl"}>
+      <Flex direction={"column"} gap={8}>
+        <Heading fontSize={"3xl"}>Settings</Heading>
+        {/* About - Start */}
+        <Stack gap={4}>
+          <Heading>Profile</Heading>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit,
+            numquam! Quas tempora est aliquid dolorem sit provident nam ducimus
+            quis autem.
+          </Text>
+        </Stack>
+        {/* About - ENd */}
+      </Flex>
+    </Container>
+  );
 }
 
 export default Leaderboard;
