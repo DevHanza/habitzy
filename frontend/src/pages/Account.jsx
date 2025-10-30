3c605b2e13902e95387a7508ea0ffc1989a642ad
DevHanza <hansana123dasanayake@gmail.com>
2025-10-30 19:41:59 +0530

    Account Page: Add a Basic Cards to show Name & Username

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,9 +1,27 @@
-import { Flex, Container } from "@chakra-ui/react";
+import { Flex, Container, Stack, Heading, Text } from "@chakra-ui/react";
 
 function Account() {
   return (
-    <Container>
-      <Flex>Account</Flex>
+    <Container maxW={"xl"}>
+      <Flex direction={"column"}>
+        {/* Profile Card - START*/}
+        <Stack
+          gap={2}
+          background={"bg.emphasized"}
+          border={"2px solid"}
+          borderColor={"fg.subtle"}
+          px={4}
+          py={8}
+          width={"100%"}
+          borderRadius={12}
+        >
+          <Heading fontSize={"1.5rem"} lineHeight={1}>
+            DevHanza
+          </Heading>
+          <Text lineHeight={1}>@devhanza</Text>
+        </Stack>
+        {/* Profile Card - END */}
+      </Flex>
     </Container>
   );
 }
