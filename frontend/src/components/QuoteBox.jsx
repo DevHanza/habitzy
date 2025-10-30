9560d7d7b163617542051e7bbbcdd98e260e484f
DevHanza <hansana123dasanayake@gmail.com>
2025-10-30 22:14:17 +0530

    Fix QuoteBox: WidgetsWrapper is not showing while loading.

--- frontend/src/components/QuoteBox.jsx
+++ frontend/src/components/QuoteBox.jsx
@@ -1,11 +1,11 @@
-import { Heading, Stack, Text, Spinner, Alert } from "@chakra-ui/react";
+import { Heading, Stack, Text, Spinner, Alert, Box } from "@chakra-ui/react";
 import WidgetsWrapper from "./ui/WidgetWrapper";
 import useFetchQuote from "@/hooks/useFetchQuote";
 
 function QuoteBox() {
   const { quote, loading, error } = useFetchQuote();
 
-  if (loading) return <Spinner color="teal.500" size="md" />;
+  // if (loading) return <Spinner color="teal.500" size="md" />;
 
   if (error)
     return (
@@ -16,19 +16,25 @@ function QuoteBox() {
     );
 
   return (
-    <WidgetsWrapper py={6}>
-      <Stack gap={4}>
-        <Heading
-          fontWeight={500}
-          size={{ base: "md", md: "lg" }}
-          lineHeight={{ base: 1.25, md: 1.3 }}
-        >
-          {quote[0].content}
-        </Heading>
-        <Text fontSize={"0.875rem"} color={"fg.muted"}>
-          — {quote[0].author}
-        </Text>
-      </Stack>
+    <WidgetsWrapper py={6} justifyItems="center">
+      {loading ? (
+        <Box gap={0} lineHeight={1}>
+          <Spinner color="teal.500" size="md" />
+        </Box>
+      ) : (
+        <Stack gap={4}>
+          <Heading
+            fontWeight={500}
+            size={{ base: "md", md: "lg" }}
+            lineHeight={{ base: 1.25, md: 1.3 }}
+          >
+            {quote[0].content}
+          </Heading>
+          <Text fontSize={"0.875rem"} color={"fg.muted"}>
+            — {quote[0].author}
+          </Text>
+        </Stack>
+      )}
     </WidgetsWrapper>
   );
 }
