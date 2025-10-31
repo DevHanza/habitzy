a22a773794f3b1d288ef09bdeb890cfc8d17d5a0
DevHanza <hansana123dasanayake@gmail.com>
2025-10-31 19:13:04 +0530

    Fix BottomNav Hide page content on mobile

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import { Container, Stack, Box, Text } from "@chakra-ui/react";
 function Footer() {
   return (
     <footer>
-      <Box py={"0.5rem"}>
+      <Box py={"0.5rem"} mb={{ base: 8, md: 0 }}>
         <Container>
           <Stack>
             <Text textAlign={"center"} fontSize={"0.75rem"} color={"fg.muted"}>
