460e751f61a39ee587b0b86663577beee603a0c4
DevHanza <hansana123dasanayake@gmail.com>
2025-11-05 10:15:36 +0530

    Rename Loader to LoadingScreen

---
+++ frontend/src/components/ui/LoadingScreen.jsx
@@ -0,0 +1,28 @@
+import { Spinner, Box, Flex } from "@chakra-ui/react";
+
+import React from "react";
+
+function LoadingScreen() {
+  return (
+    <Box
+      position={"fixed"}
+      inset={0}
+      zIndex={9999}
+      backgroundColor={"black"}
+      height={"100vh"}
+      width={"100%"}
+      overflow={"hidden"}
+    >
+      <Flex alignItems={"center"} justifyContent={"center"} height={"100%"}>
+        <Spinner
+          size={{ base: "md", md: "xl" }}
+          colorPalette={"teal"}
+          alignContent={"center"}
+          color="colorPalette.300"
+        />
+      </Flex>
+    </Box>
+  );
+}
+
+export default LoadingScreen;
