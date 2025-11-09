eab9952b4f42a85a757b4e01bd0bcbc9e434317b
DevHanza <hansana123dasanayake@gmail.com>
2025-11-09 17:49:36 +0530

    Add AuthPage Template for pages like signup/login

--- frontend/src/components/layout/AuthPage.jsx
+++ frontend/src/components/layout/AuthPage.jsx
@@ -1,37 +1,96 @@
-import { Flex, Image, Stack, Container } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Image,
+  Stack,
+  Container,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { ArrowLeft } from "lucide-react";
+import { Link, useNavigate } from "react-router";
 
-function AuthPage({ children }) {
+function AuthPage({
+  children,
+  heading = "Create an Account",
+  headingText = (
+    <Text color={"fg.muted"} textAlign={{ base: "center", lg: "left" }}>
+      Already have an account?{" "}
+      <Link to="/login" style={{ textDecoration: "underline", color: "white" }}>
+        Log in
+      </Link>
+    </Text>
+  ),
+  isNavVisible = true,
+}) {
+  const navigate = useNavigate();
+
   return (
-    <Flex>
-      {/* Background Two Column Layout */}
-      <Flex
-        height={"100%"}
-        maxHeight={"100vh"}
-        p={2}
-        gap={8}
-        position={"absolute"}
-        inset={0}
-        zIndex={-1}
-      >
-        <Stack flex={1}>
-          <Image
-            src="/login-screen-1.jpg"
-            h={"100%"}
-            w={"100%"}
-            fit={"cover"}
-            borderRadius={"lg"}
-          ></Image>
+    <Container height={"100vh"} maxW={{ base: "lg", lg: "5xl" }}>
+      <Flex justifyItems={"center"} alignItems={"Center"} height={"100%"}>
+        <Stack
+          width={"full"}
+          height={{ base: "auto", lg: "80vh" }}
+          maxHeight={"600px"}
+          direction={{ base: "column", lg: "row" }}
+          borderRadius={"xl"}
+          overflow={"hidden"}
+          border={"1px solid"}
+          borderColor={"border.emphasized"}
+          gap={0}
+        >
+          <Stack
+            flex={1}
+            bg={"bg.emphasized"}
+            display={{ base: "none", lg: "flex" }}
+          >
+            <Image
+              src="/login-screen-1.jpg"
+              height={"100%"}
+              w={"100%"}
+              fit={"cover"}
+            ></Image>
+          </Stack>
+          <Stack flex={1.25}>
+            <Box
+              px={{ base: 6, lg: 16 }}
+              py={{ base: 12, lg: 20 }}
+              display={"flex"}
+              height={"100%"}
+            >
+              <Stack width={"100%"} gap={16}>
+                {/* <Stack width={"fit-content"} p={2}>
+                  {isNavVisible && (
+                    <Link to={() => navigate(-1)}>
+                      <ArrowLeft size={20} />
+                    </Link>
+                  )}
+                </Stack> */}
+                <Stack gap={8}>
+                  <Stack gap={1} alignItems={{ base: "center", lg: "start" }}>
+                    <Image
+                      src="/habits-2.svg"
+                      boxSize="3rem"
+                      textAlign={"center"}
+                      mb={4}
+                      display={{ base: "block", lg: "none" }}
+                    />
+                    <Heading
+                      size={{ base: "2xl", lg: "4xl" }}
+                      textAlign={{ base: "center", lg: "left" }}
+                    >
+                      {heading}
+                    </Heading>
+                    {headingText}
+                  </Stack>
+                  {children}
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
         </Stack>
-        <Stack flex={1}></Stack>
       </Flex>
-      {/* Background Two Column Layout */}
-      <Container maxHeight={"100vh"}>
-        <Flex height={"100vh"} alignItems={"center"} gap={8}>
-          <Stack flex={1}></Stack>
-          <Stack flex={1}>{children}</Stack>
-        </Flex>
-      </Container>
-    </Flex>
+    </Container>
   );
 }
 
