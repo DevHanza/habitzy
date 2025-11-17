d7ea189421c00dbe1e73fe90addf7d113b2646b7
DevHanza <hansana123dasanayake@gmail.com>
2025-11-17 16:11:19 +0530

    Add a design to ErrorPage

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,11 +1,43 @@
-import { Heading } from "@chakra-ui/react";
-import { useRouteError } from "react-router";
+import {
+  Container,
+  Heading,
+  Text,
+  Image,
+  Button,
+  Flex,
+  Stack,
+} from "@chakra-ui/react";
+import { Link, useRouteError } from "react-router";
 
 function ErrorPage() {
   const error = useRouteError();
   console.log(error);
 
-  return <Heading>ERROR OCCURED!</Heading>;
+  return (
+    <Container maxW={"xl"}>
+      <Flex height={"100vh"} justifyContent={"center"} alignItems={"center"}>
+        <Stack alignItems={"center"}>
+          <Image
+            src="https://emojicdn.elk.sh/⁉️?style=facebook"
+            height={{ base: "3rem", md: "3.5rem" }}
+            width={"fit-content"}
+            marginBottom={5}
+          />
+          <Heading size={{ base: "2xl", md: "3xl" }}>
+            Something Went Wrong!
+          </Heading>
+          <Text textAlign={"center"} color={"fg.subtle"}>
+            Oops! We couldn’t find that page. You can go back to the homepage.
+          </Text>
+          <Link to={"/"}>
+            <Button marginTop={5} colorPalette={"teal"}>
+              Go to Homepage
+            </Button>
+          </Link>
+        </Stack>
+      </Flex>
+    </Container>
+  );
 }
 
 export default ErrorPage;
