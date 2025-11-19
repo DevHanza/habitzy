c0f6c6c58fb067bebccc1d03501498bc361030b0
DevHanza <hansana123dasanayake@gmail.com>
2025-11-19 09:41:59 +0530

    Settings Page: Add Dialog Box to confirm"Delete Account"

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -14,6 +14,9 @@ import {
   Separator,
   Button,
   Switch,
+  CloseButton,
+  Dialog,
+  Portal,
 } from "@chakra-ui/react";
 
 import { Link } from "react-router";
@@ -102,7 +105,7 @@ function Preferences() {
           colorPalette={"teal"}
           checked={colorMode === "dark" ? true : false}
         >
-          <Switch.HiddenInput onClick={toggleColorMode}/>
+          <Switch.HiddenInput onClick={toggleColorMode} />
           <Switch.Control />
           <Switch.Label>Enable Dark Mode</Switch.Label>
         </Switch.Root>
@@ -125,14 +128,44 @@ function DeleteAccountSettings() {
           erased. We don't store backups, so be sure to save anything important
           first.
         </Text>
-        <Button
-          size={"sm"}
-          width={"fit-content"}
-          colorPalette={"red"}
-          variant="subtle"
-        >
-          Delete Account
-        </Button>
+
+        <Dialog.Root role="alertdialog" placement={"center"}>
+          <Dialog.Trigger asChild>
+            <Button
+              size={"sm"}
+              width={"fit-content"}
+              colorPalette={"red"}
+              variant="subtle"
+            >
+              Delete Account
+            </Button>
+          </Dialog.Trigger>
+          <Portal>
+            <Dialog.Backdrop />
+            <Dialog.Positioner>
+              <Dialog.Content>
+                <Dialog.Header>
+                  <Dialog.Title>Are you sure?</Dialog.Title>
+                </Dialog.Header>
+                <Dialog.Body>
+                  <p>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our systems.
+                  </p>
+                </Dialog.Body>
+                <Dialog.Footer>
+                  <Dialog.ActionTrigger asChild>
+                    <Button variant="outline">Cancel</Button>
+                  </Dialog.ActionTrigger>
+                  <Button colorPalette="red">Delete</Button>
+                </Dialog.Footer>
+                <Dialog.CloseTrigger asChild>
+                  <CloseButton size="sm" />
+                </Dialog.CloseTrigger>
+              </Dialog.Content>
+            </Dialog.Positioner>
+          </Portal>
+        </Dialog.Root>
       </Stack>
     </Stack>
   );
