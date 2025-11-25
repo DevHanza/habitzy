a502f726229823bd1a7fc0ad7ee8d8fca1fd82fb
DevHanza <hansana123dasanayake@gmail.com>
2025-11-25 09:47:13 +0530

    Change Global rate limit for api routes from "100 req. per 15 min" to "100 req. per 1min"

--- backend/src/middleware/rate-limiter.js
+++ backend/src/middleware/rate-limiter.js
@@ -1,7 +1,7 @@
 import { rateLimit } from "express-rate-limit";
 
 export const globalLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 mins
+  windowMs: 1 * 60 * 1000, // 1 mins
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true,
   legacyHeaders: false,
