d3e55915cc2e12c0fab5b6e3895a581eb1a4e864
DevHanza <hansana123dasanayake@gmail.com>
2025-11-25 09:36:57 +0530

    Add more functionality to the rate limiter and commented the code (for future use)

--- backend/src/middleware/rate-limiter.js
+++ backend/src/middleware/rate-limiter.js
@@ -5,4 +5,14 @@ export const globalLimiter = rateLimit({
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true,
   legacyHeaders: false,
+  // message: {
+  //   error: "Too many requests. please try again later.",
+  // },
 });
+
+// export const loginLimiter = rateLimit({
+//   windowMs: 10 * 60 * 1000, // 10 mins
+//   limit: 20,
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
