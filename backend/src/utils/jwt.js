938610a4fa576168a733f97b7af0da3917b6b21e
DevHanza <hansana123dasanayake@gmail.com>
2025-11-27 11:03:01 +0530

    Change the code to match the changed .env variable names

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_ACCESS_TOKEN_SECRET, {
+  return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: "15m",
   });
 };
 
 export const generateRefreshToken = (userId, device) => {
-  return jwt.sign({ userId, device }, process.env.JWT_REFRESH_TOKEN_SECRET, {
+  return jwt.sign({ userId, device }, process.env.REFRESH_TOKEN_SECRET, {
     expiresIn: "7d",
   });
 };
 
 export const verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_REFRESH_TOKEN_SECRET);
+  return jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
 };
