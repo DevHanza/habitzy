2d86681a218d6aeb02d783481bd4903642d4cfd1
DevHanza <hansana123dasanayake@gmail.com>
2025-11-30 13:09:33 +0530

    Fix userModel: Change verifyCodes.code to type 'String'.

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -60,7 +60,7 @@ const userSchema = new Schema(
     verifyCodes: {
       type: [
         {
-          code: Number,
+          code: String,
           device: String,
           expiresAt: Date,
         },
