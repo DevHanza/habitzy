84d1307545d7b2a0adbbe0535d36ba1a074a8d63
DevHanza <hansana123dasanayake@gmail.com>
2025-12-02 12:21:16 +0530

    Redirect to '/verify' after ''/forgot-password' with required data

--- frontend/src/components/Auth/ForgotPasswordInputs.jsx
+++ frontend/src/components/Auth/ForgotPasswordInputs.jsx
@@ -1,22 +1,58 @@
-import { Stack, Field, Input, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { Stack, Field, Input, Button, Spinner } from "@chakra-ui/react";
 
 function ForgotPasswordInputs() {
-  return (
-    <Stack gap={4}>
-      <Stack gap={3}>
-        {/* Inputs */}
-        <Field.Root required>
-          <Input
-            borderColor={"border.emphasized"}
-            placeholder="Email Address"
-            variant="subtle"
-            colorPalette={"teal"}
-          />
-        </Field.Root>
-      </Stack>
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-      <Button>Send Verification Code</Button>
-    </Stack>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    try {
+      //
+      navigate("/verify", { state: { email: event.target.email.value } });
+      //
+    } catch (err) {
+      //
+      console.log(err);
+      //
+    } finally {
+      //
+      setIsLoading(false);
+      //
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Stack gap={4}>
+        <Stack gap={3}>
+          {/* Inputs */}
+          <Field.Root required>
+            <Input
+              name="email"
+              type="email"
+              borderColor={"border.emphasized"}
+              placeholder="Email Address"
+              variant="subtle"
+              colorPalette={"teal"}
+            />
+          </Field.Root>
+        </Stack>
+
+        <Button type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <>
+              <Spinner size={"sm"} />
+              Loading...
+            </>
+          ) : (
+            "Send Verification Code"
+          )}
+        </Button>
+      </Stack>
+    </form>
   );
 }
 
--- frontend/src/pages/Auth/VerifyCode.jsx
+++ frontend/src/pages/Auth/VerifyCode.jsx
@@ -2,14 +2,18 @@ import { Text } from "@chakra-ui/react";
 
 import AuthPage from "@/components/layout/AuthPage";
 import VerifyCodeInputs from "@/components/Auth/VerifyCodeInputs";
+import { useLocation } from "react-router";
 
 function VerifyCode() {
+  const location = useLocation();
+  const email = location.state?.email;
+  
   return (
     <AuthPage
       heading="Verification Code"
       headingText={
         <Text color={"fg.muted"} textAlign={{ base: "center", lg: "left" }}>
-          We sent you verification code on ....<b>devhanza@gmail.com</b>
+          We sent you verification code on ....<b>{email}</b>
         </Text>
       }
     >
