f81d5651a8306c1ebf09b78d8fd908dd18283da4
DevHanza <hansana123dasanayake@gmail.com>
2025-12-08 19:12:25 +0530

    Rename AuthProvider.js into .jsx

--- frontend/src/context/AuthProvider.js
+++
@@ -1,6 +0,0 @@
-import { AuthContext } from "@/context/AuthContext";
-
-export const AuthProvider = ({ children}) => {
-
-    return <AuthContext.Provider value={}>{children}</AuthContext.Provider>
-}
---
+++ frontend/src/context/AuthProvider.jsx
@@ -0,0 +1,39 @@
+import { useReducer } from "react";
+import { AuthContext } from "@/context/AuthContext";
+import { loginRequest } from "@/api/authAPI";
+
+const initialState = {
+  user: null,
+  accessToken: null,
+};
+
+function reducer(state, payload) {
+  switch (action.type) {
+    case "SET_TOKEN":
+      return { ...state, accessToken: action.payload };
+    default:
+      return state;
+  }
+}
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  async function login(email, password) {
+    const res = await loginRequest(email, password);
+    if (!res.ok) throw Error(data.message);
+
+    const data = res.json();
+    dispatch({ type: "SET_TOKEN", payload: data.accessToken });
+
+    return true;
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ state, accessToken: state.accessToken, login }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
