4e817c284a2d345436e89cbba99095cd0822ea69
DevHanza <hansana123dasanayake@gmail.com>
2025-12-08 17:36:11 +0530

    Add "AuthContext" & AuthProvider

---
+++ frontend/src/context/AuthProvider.js
@@ -0,0 +1,6 @@
+import { AuthContext } from "@/context/AuthContext";
+
+export const AuthProvider = ({ children}) => {
+
+    return <AuthContext.Provider value={}>{children}</AuthContext.Provider>
+}
