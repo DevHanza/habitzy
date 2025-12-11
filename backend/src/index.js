ba4a2b09723a7c5b2cc2f2b6fbf6c05e875bd233
DevHanza <hansana123dasanayake@gmail.com>
2025-12-11 09:39:06 +0530

    Fix CORS Issue in the backend. By Setting up the dotenv library properly.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import userRoutes from "./routes/userRoutes.js";
 import habitRoutes from "./routes/habitRoutes.js";
 import dailyLogRoutes from "./routes/dailyLogRoutes.js";
@@ -10,18 +13,19 @@ import { globalLimiter } from "./middleware/rate-limiter.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(globalLimiter);
+
+// CORS
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], //
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Load routes
 app.use("/api/v1/user", userRoutes);
