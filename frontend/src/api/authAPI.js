bc99c4f3fcf0c9b49c921a168f4b716e5bb75ffc
DevHanza <hansana123dasanayake@gmail.com>
2025-12-11 10:05:08 +0530

    loginRequest() for AuthProvider

---
+++ frontend/src/api/authAPI.js
@@ -0,0 +1,10 @@
+const API = `${import.meta.env.VITE_BACKEND_API}/user`;
+
+export const loginRequest = (email, password) =>
+  fetch(`${API}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({ email, password }),
+  });
+
