d476d806626a17d999be62d1cec9bf7de1577415
DevHanza <hansana123dasanayake@gmail.com>
2025-12-14 16:09:31 +0530

    Add the functional validations in the frontend with handleSubmit() for the form in LoginInputs.jsx

--- frontend/src/components/Auth/LoginInputs.jsx
+++ frontend/src/components/Auth/LoginInputs.jsx
@@ -7,7 +7,50 @@ function LoginInputs() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
+  async function handleSubmit(formData) {
+    setLoading(true);
+    try {
+      //
+      let email = formData.get("email").trim().toLowerCase();
+      let pass = formData.get("password").trim();
 
+      const emailRegex = /^[^@\s+]+@[^@\s]+\.[^@\s]+$/;
+
+      if (!email || email === "") {
+        //
+        return setError("Email is required.");
+        //
+      } else if (email.length < 5) {
+        //
+        return setError("Email must be longer than 5 characters.");
+        //
+      } else if (!emailRegex.test(email)) {
+        //
+        return setError("Please enter a valid email address.");
+        //
+      }
+
+      if (!pass || pass === "") {
+        //
+        return setError("Password is required.");
+        //
+      } else if (pass.length < 5 || pass.length > 10) {
+        //
+        return setError("Invalid password.");
+        //
+      }
+
+      //
+    } catch (err) {
+      //
+      setLoading(false);
+      setError("Error! Authentication failed.");
+      // console.log(err);
+      //
+    } finally {
+      // setLoading(false);
+    }
+  }
 
   return (
     <Stack gap={4} asChild>
