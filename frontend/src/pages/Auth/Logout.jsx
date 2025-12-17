ec7fe80802f2b6b8b831dc1b4b5ca0e94a4b4906
DevHanza <hansana123dasanayake@gmail.com>
2025-12-17 19:20:36 +0530

    Implement logout functionality for "/logout" (frontend)

--- frontend/src/pages/Auth/Logout.jsx
+++ frontend/src/pages/Auth/Logout.jsx
@@ -1,6 +1,38 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useAuth } from "@/hooks/useAuth";
 import LoadingScreen from "@/components/ui/LoadingScreen";
 
 function Logout() {
+  const navigate = useNavigate();
+  const { isLoggedIn, logout } = useAuth();
+
+  useEffect(() => {
+    throw Error("test");
+    //
+    if (!isLoggedIn) {
+      console.log("You must log in first, order to logout.");
+      navigate("/");
+      return;
+    }
+
+    try {
+      logout()
+        .then((data) => {
+          console.log(data.message);
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err.message);
+          navigate("/");
+        });
+    } catch (err) {
+      throw Error(err.message);
+    }
+
+    //
+  });
+
   return <LoadingScreen />;
 }
 
