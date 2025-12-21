b3f75e5ffffec276b61c872fab532f1efbb54083
DevHanza <hansana123dasanayake@gmail.com>
2025-12-21 22:43:31 +0530

    Add form validations: ForgotPasswordInputs

--- frontend/src/components/Auth/ForgotPasswordInputs.jsx
+++ frontend/src/components/Auth/ForgotPasswordInputs.jsx
@@ -6,12 +6,30 @@ function ForgotPasswordInputs() {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
     try {
       //
-      navigate("/verify", { state: { email: event.target.email.value } });
+      const email = e.target.elements.email.value.trim().toLowerCase();
+
+      const emailRegex = /^[^@\s+]+@[^@\s]+\.[^@\s]+$/;
+
+      if (!email || email.trim() === "") {
+        //
+        throw new Error("Email is required.");
+        //
+      } else if (email.length < 5) {
+        //
+        throw new Error("Email must be longer than 5 characters.");
+        //
+      } else if (!emailRegex.test(email)) {
+        //
+        throw new Error("Please enter a valid email address.");
+        //
+      }
+      navigate("/verify", { state: { email: email } });
       //
     } catch (err) {
       //
