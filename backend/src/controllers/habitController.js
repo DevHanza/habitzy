9e12acea2565cd05c84ba132945ce2650d28796e
DevHanza <hansana123dasanayake@gmail.com>
2025-12-29 21:15:24 +0530

    Remove (comment) "getHabitByID" from backend

--- backend/src/controllers/habitController.js
+++ backend/src/controllers/habitController.js
@@ -16,25 +16,6 @@ export async function getHabits(req, res) {
   }
 }
 
-//
-export async function getHabitByID(req, res) {
-  try {
-    const { userId, habitId } = req.params;
-    const habit = await Habit.find({
-      _id: habitId,
-      userId: userId,
-    });
-
-    if (!habit) {
-      return res.status(404).json({ message: "No habit found." });
-    }
-
-    res.json(habit);
-  } catch (errr) {
-    res.status(500).json({ message: err.message });
-  }
-}
-
 export async function addHabit(req, res) {
   try {
     const { userId } = req.params;
@@ -108,3 +89,22 @@ export async function updateHabit(req, res) {
     res.status(400).json({ message: err.message });
   }
 }
+
+//
+// export async function getHabitByID(req, res) {
+//   try {
+//     const { userId, habitId } = req.params;
+//     const habit = await Habit.find({
+//       _id: habitId,
+//       userId: userId,
+//     });
+
+//     if (!habit) {
+//       return res.status(404).json({ message: "No habit found." });
+//     }
+
+//     res.json(habit);
+//   } catch (errr) {
+//     res.status(500).json({ message: err.message });
+//   }
+// }
