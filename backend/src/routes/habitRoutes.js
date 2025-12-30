ee9b108d27bb8eb01418be1ae8fbd962ed68cb9e
DevHanza <hansana123dasanayake@gmail.com>
2025-12-30 14:28:37 +0530

    Comment some routes in habitRoutes for now (backend)

--- backend/src/routes/habitRoutes.js
+++ backend/src/routes/habitRoutes.js
@@ -3,21 +3,17 @@ import { Router } from "express";
 import {
   getHabits,
   // getHabitByID,
-  addHabit,
-  deleteHabit,
-  updateHabit,
+  // deleteHabit,
+  // updateHabit,
 } from "../controllers/habitController.js";
 
 // Enable "mergeParams" so we can access ":id" from parent route
 const router = Router({ mergeParams: true });
 
 router.get("/", getHabits);
+
 // router.get("/:habitId", getHabitByID);
-
-router.post("/", addHabit);
-
-router.delete("/:habitId", deleteHabit);
-
-router.put("/:habitId", updateHabit);
+// router.delete("/:habitId", deleteHabit);
+// router.put("/:habitId", updateHabit);
 
 export default router;
