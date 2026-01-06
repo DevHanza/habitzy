65b575a85777968bdb209b996430c241516122c3
DevHanza <hansana123dasanayake@gmail.com>
2026-01-06 22:24:18 +0530

    Comment some unused code for user routes. (backend)

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -540,38 +540,38 @@ export async function resetPassword(req, res) {
   }
 }
 
-export async function getUserByID(req, res) {
-  const { userId } = req.params;
+// export async function getUserByID(req, res) {
+//   const { userId } = req.params;
 
-  try {
-    const user = await User.findById(userId);
+//   try {
+//     const user = await User.findById(userId);
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found." });
-    }
-    //
-    res.status(200).json(user);
-    //
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-}
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found." });
+//     }
+//     //
+//     res.status(200).json(user);
+//     //
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// }
 
-export async function updateUser(req, res) {
-  try {
-    const { userId } = req.params;
-    const newUserData = req.body;
+// export async function updateUser(req, res) {
+//   try {
+//     const { userId } = req.params;
+//     const newUserData = req.body;
 
-    const updatedUser = await User.findByIdAndUpdate(userId, newUserData, {
-      new: true,
-      runValidators: true,
-    });
+//     const updatedUser = await User.findByIdAndUpdate(userId, newUserData, {
+//       new: true,
+//       runValidators: true,
+//     });
 
-    if (!updateUser) {
-      return res.status(404).json({ message: "User not found." });
-    }
-    res.json(updatedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-}
+//     if (!updateUser) {
+//       return res.status(404).json({ message: "User not found." });
+//     }
+//     res.json(updatedUser);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// }
