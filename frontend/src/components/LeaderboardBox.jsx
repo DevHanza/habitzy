4774f4f648d4bd984ff428b5b5352b4c47f7786a
DevHanza <hansana123dasanayake@gmail.com>
2026-01-14 16:11:54 +0530

    Fetch the Leaderboard from the API and & Save on the frontend

--- frontend/src/components/LeaderboardBox.jsx
+++ frontend/src/components/LeaderboardBox.jsx
@@ -1,21 +1,28 @@
 import { VStack } from "@chakra-ui/react";
 import WidgetsWrapper from "@/components/ui/WidgetWrapper";
 import LeaderboardCard from "@/components/LeaderboardCard";
-
-const users = [
-  { name: "Brook Lesnar", username: "brookkiller", streak: 45 },
-  { name: "Diego Ramirez", username: "drz_90", streak: 34 },
-  { name: "Alice Wong", username: "awong23", streak: 27 },
-  { name: "Hana Kimura", username: "hanak", streak: 22 },
-  { name: "Sophia Turner", username: "sophiat", streak: 19 },
-  { name: "Liam Carter", username: "lcarter7", streak: 16 },
-  { name: "Shane Nelson", username: "shanethedev", streak: 12 },
-  { name: "Marcus Reid", username: "mr_reid", streak: 8 },
-  { name: "Emma Brooks", username: "emmbrooks", streak: 5 },
-  { name: "John Lennon", username: "johnnybeat", streak: 3 },
-];
+import { useEffect, useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
 
 function LeaderboardBox() {
+  const { authFetch } = useAuth();
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    //
+    authFetch({
+      url: "user/leaderboard",
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        setUsers(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //
+  }, [authFetch, setUsers]);
+
   return (
     <WidgetsWrapper
       title={"Leaderboard"}
@@ -25,11 +32,11 @@ function LeaderboardBox() {
       <VStack gap={2}>
         {users.map((user, index) => (
           <LeaderboardCard
-            key={user.username}
+            key={user._id}
             rank={index + 1}
             name={user.name}
             username={user.username}
-            streak={user.streak}
+            streak={user.streak.currentStreak}
           />
         ))}
       </VStack>
