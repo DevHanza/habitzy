133c755af8b95f9f69f7f7d1a457b14332e0d1cc
DevHanza <hansana123dasanayake@gmail.com>
2026-01-16 22:40:36 +0530

    Renam avatar menu item "Profile" to "Account" in header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -73,7 +73,7 @@ const bottomNavItems = [
 ];
 
 const avatarMenuItems = [
-  { label: "Profile", link: "/account", icon: User },
+  { label: "Account", link: "/account", icon: User },
   { label: "Settings", link: "/settings", icon: Settings },
 ];
 
