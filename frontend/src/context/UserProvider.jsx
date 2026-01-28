14ae0c6c8916ba65995d3a3b73d09b9dbdaa83b3
DevHanza <hansana123dasanayake@gmail.com>
2026-01-28 16:08:38 +0530

    Reimplement the UserProvider for new structure (frontend)

--- frontend/src/context/UserProvider.jsx
+++ frontend/src/context/UserProvider.jsx
@@ -1,32 +1,122 @@
 import { UserContext } from "@/context/UserContext";
-import { useState } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import { useEffect, useReducer } from "react";
 
-const userData = {
-  name: "Dev Hanza",
-  email: "hello@hansana.is-a.dev",
-  username: "DevHanza",
-  currentStreak: 21,
-  longestStreak: 0,
-  createdAt: "2025-11-17T15:13:25.553+00:00",
+const initialState = {
+  user: null,
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    //
+    case "SET_USER":
+      return {
+        ...action.payload.user,
+      };
+    //
+    case "UPDATE_USER":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload,
+        },
+      };
+    //
+    case "INCREMENT_STREAK":
+      return {
+        ...state,
+        currentStreak: state.currentStreak++,
+      };
+    //
+    case "CLEAR_USER":
+      return initialState;
+    //
+    default:
+      return state;
+  }
+}
+
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(userData);
+  const [state, userDispatch] = useReducer(reducer, initialState);
+  const { isLoggedIn, authFetch } = useAuth();
+
+  //
+  //
+  //
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+    //
+    const fetchUser = async () => {
+      const response = await authFetch({
+        url: "user",
+        method: "GET",
+      });
+
+      //
+      const data = await response.json();
+      // console.log(data);
+      userDispatch({
+        type: "SET_USER",
+        payload: {
+          user: data.user,
+        },
+      });
+    };
+
+    fetchUser();
+    //
+  }, [authFetch, isLoggedIn]);
+
+  //
+  //
+  //
+
+  async function updateUser(updatedUserProp) {
+    try {
+      //
+
+      authFetch({
+        url: "user",
+        method: "PATCH",
+        body: { ...updatedUserProp },
+      })
+        .then(async () =>
+          // response
+          {
+            // const data = await response.json();
+            //
+            userDispatch({
+              type: "UPDATE_USER",
+              payload: {
+                payload: updatedUserProp,
+              },
+            });
+
+            //
+          },
+        )
+        .catch((err) => {
+          console.log(err);
+        });
+
+      //
+    } catch (err) {
+      throw Error(err);
+    }
+  }
 
   const incrementStreak = () => {
-    const longestStreak = Math.max(user.currentStreak, user.longestStreak);
-
-    setUser((prevUser) => {
-      return {
-        ...prevUser,
-        currentStreak: prevUser.currentStreak + 1,
-        longestStreak,
-      };
+    userDispatch({
+      type: "INCREMENT_STREAK",
     });
   };
 
   return (
-    <UserContext.Provider value={{ user, setUser, incrementStreak }}>
+    <UserContext.Provider
+      value={{ user: state, userDispatch, updateUser, incrementStreak }}
+    >
       {children}
     </UserContext.Provider>
   );
