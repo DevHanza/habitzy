a4a7bfc8faada1bca395f9912074b06527427fe0
DevHanza <hansana123dasanayake@gmail.com>
2026-02-02 14:55:43 +0530

    Disable react-compiler in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,12 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(
-      {
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }
-  ),
+    react(),
+    //   {
+    //   babel: {
+    //     plugins: ["babel-plugin-react-compiler"],
+    //   },
+    // }
     tsconfigPaths(),
   ],
 });
