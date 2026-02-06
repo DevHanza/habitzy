897ecb14968c0f6a1bb0664a08909a76e935b367
DevHanza <hansana123dasanayake@gmail.com>
2026-02-06 22:10:20 +0530

    Add loading Skeleton to StreakBox

--- frontend/src/components/StreakBox.jsx
+++ frontend/src/components/StreakBox.jsx
@@ -1,6 +1,14 @@
 import { useMemo, memo, useEffect } from "react";
 import WidgetWrapper from "./ui/WidgetWrapper";
-import { HStack, VStack, Stack, Image, Heading, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  VStack,
+  Stack,
+  Image,
+  Heading,
+  Text,
+  Skeleton,
+} from "@chakra-ui/react";
 import useHabits from "@/hooks/useHabits";
 import { useUser } from "@/hooks/useUser";
 
@@ -8,7 +16,7 @@ import runOncePerDay from "@/utils/runOncePerDay";
 
 function StreakBox() {
   const { habits } = useHabits();
-  const { user, incrementStreak } = useUser();
+  const { user, isUserLoading, incrementStreak } = useUser();
 
   const hasStreak = user?.streak?.currentStreak > 0;
 
@@ -28,33 +36,41 @@ function StreakBox() {
   return (
     <WidgetWrapper py={6}>
       <Stack>
-        <HStack gap={1} alignItems={"center"} justifyContent={"center"}>
-          <Heading
-            display={"inline-block"}
-            size={{ base: "3xl", md: "5xl" }}
-            fontWeight={700}
-            lineHeight={1}
-            letterSpacing={1}
-          >
-            {user?.streak?.currentStreak}
-          </Heading>
-          <Image
-            src={
-              hasStreak
-                ? "https://emojicdn.elk.sh/🔥?style=facebook"
-                : "https://emojicdn.elk.sh/🌱?style=facebook"
-            }
-            height={{ base: "1.75rem", md: "2.75rem" }}
-          />
-        </HStack>
+        <Skeleton loading={isUserLoading}>
+          <HStack gap={1} alignItems={"center"} justifyContent={"center"}>
+            <Heading
+              display={"inline-block"}
+              size={{ base: "3xl", md: "5xl" }}
+              fontWeight={700}
+              lineHeight={1}
+              letterSpacing={1}
+            >
+              {user?.streak?.currentStreak}
+            </Heading>
+            <Image
+              src={
+                hasStreak
+                  ? "https://emojicdn.elk.sh/🔥?style=facebook"
+                  : "https://emojicdn.elk.sh/🌱?style=facebook"
+              }
+              height={{ base: "1.75rem", md: "2.75rem" }}
+            />
+          </HStack>
+        </Skeleton>
 
         <VStack gap={0}>
-          <Heading size={{ base: "lg", md: "xl" }}>
-            {hasStreak ? "Keep the streak alive!" : "Start a streak!"}
-          </Heading>
-          <Text color={"fg.muted"}>
-            {hasStreak ? "Consistency is power." : "Consistency starts today."}
-          </Text>
+          <Skeleton loading={isUserLoading}>
+            <Heading size={{ base: "lg", md: "xl" }}>
+              {hasStreak ? "Keep the streak alive!" : "Start a streak!"}
+            </Heading>
+          </Skeleton>
+          <Skeleton loading={isUserLoading}>
+            <Text color={"fg.muted"}>
+              {hasStreak
+                ? "Consistency is power."
+                : "Consistency starts today."}
+            </Text>
+          </Skeleton>
         </VStack>
       </Stack>
     </WidgetWrapper>
