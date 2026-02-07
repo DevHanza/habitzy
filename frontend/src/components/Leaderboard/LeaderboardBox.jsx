1ea03130f1fdd496f41b7f4bc3682f03711c410c
DevHanza <hansana123dasanayake@gmail.com>
2026-02-07 10:58:19 +0530

    Refactor and show skeletonLeaderboardCards while loading in LeaderboardBox (frontend)

--- frontend/src/components/Leaderboard/LeaderboardBox.jsx
+++ frontend/src/components/Leaderboard/LeaderboardBox.jsx
@@ -5,26 +5,56 @@ import LeaderboardCard from "@/components/Leaderboard/LeaderboardCard";
 import LeaderboardBoxEmpty from "@/components/Leaderboard/LeaderboardBoxEmpty";
 
 import { useAuth } from "@/hooks/useAuth";
-
+import LeaderboardCardSkeleton from "@/components/Leaderboard/LeaderboardCardSkeleton";
 
 function LeaderboardBox() {
   const { authFetch } = useAuth();
   const [leadUsers, setLeadUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const hasleadUsers = leadUsers.length > 0;
 
   useEffect(() => {
-    //
     authFetch({
       url: "user/leaderboard",
     })
       .then(async (response) => {
         const data = await response.json();
         setLeadUsers(data.slice(0, 20));
+        setIsLoading(false);
       })
       .catch((err) => {
+        setIsLoading(false);
         console.log(err);
       });
-    //
   }, [authFetch, setLeadUsers]);
+
+  //
+
+  const skeletonLeaderboardCards = Array.from({ length: 8 }, (_, index) => (
+    <LeaderboardCardSkeleton key={index} />
+  ));
+
+  const LeaderboardCards = leadUsers.map((user, index) => (
+    <LeaderboardCard
+      key={user._id}
+      rank={index + 1}
+      name={user.name}
+      username={user.username}
+      streak={user.streak.currentStreak}
+    />
+    //
+  ));
+
+  const renderLeaderboardCards = () => {
+    if (isLoading) return skeletonLeaderboardCards;
+
+    if (hasleadUsers) {
+      return LeaderboardCards;
+    } else {
+      return skeletonLeaderboardCards;
+    }
+  };
 
   return (
     <WidgetsWrapper
@@ -32,21 +62,7 @@ function LeaderboardBox() {
       linkText={"See more"}
       link={"/leaderboard"}
     >
-      <VStack gap={2}>
-        {users.length >= 1 ? (
-              leadUsers.map((user, index) => (
-            <LeaderboardCard
-              key={user._id}
-              rank={index + 1}
-              name={user.name}
-              username={user.username}
-              streak={user.streak.currentStreak}
-            />
-          ))
-        ) : (
-          <LeaderboardBoxEmpty />
-        )}
-      </VStack>
+      <VStack gap={1}>{renderLeaderboardCards()}</VStack>
     </WidgetsWrapper>
   );
 }
