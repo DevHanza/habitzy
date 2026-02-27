1a66d8a6f9ca9d21d8d6c9be917dda74f1ca681d
DevHanza <hansana123dasanayake@gmail.com>
2026-02-27 07:50:26 +0530

    Fix normalizeDate.js files

--- backend/src/utils/normalizeDate.js
+++ backend/src/utils/normalizeDate.js
@@ -1,6 +1,7 @@
 function normalizeDate(date = new Date()) {
-  date.setHours(0, 0, 0, 0);
-  return date;
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
 }
 
 export default normalizeDate;
