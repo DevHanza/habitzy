6b111119ea5643b99282052217760de55106e286
DevHanza <hansana123dasanayake@gmail.com>
2026-02-27 10:22:33 +0530

    Fix runOncePerDay: Use ISO time instead of normal date

--- frontend/src/utils/runOncePerDay.js
+++ frontend/src/utils/runOncePerDay.js
@@ -1,17 +1,20 @@
+import normalizeDate from "./normalizeDate";
+
 function runOncePerDay(storageKey, callback) {
   if (!storageKey) {
     throw new Error("useOncePerDay: A unique 'key' must be provided");
   }
 
   const lastRun = localStorage.getItem(storageKey);
-  const today = new Date().toDateString();
+  const today = normalizeDate().toISOString();
 
   if (!lastRun || lastRun !== today) {
     callback();
     localStorage.setItem(storageKey, today);
-    return;
+    return true;
   }
-  // console.log("You've already ran this function for today.");
+  console.log("You've already ran this function for today.");
+  return false;
 }
 
 export default runOncePerDay;
