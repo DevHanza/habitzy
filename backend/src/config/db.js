d064d7b7816e2172eea1ae88cf7cb0c8c22ac14a
DevHanza <hansana123dasanayake@gmail.com>
2026-06-14 15:16:41 +0530

    Rename env variable: 'MONGO_URI' --> 'MONGODB_URI''

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,7 @@ export const connectDB = async () => {
   //
   try {
     
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: process.env.DB_NAME,
     });
 
