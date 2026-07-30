ce5fe1ab273364373a044f63ca67fd2979f49015
DevHanza <hansana123dasanayake@gmail.com>
2026-07-30 15:58:52 +0530

    chore(vite): enable babel-plugin-react-compiler

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
   base: "/",
   plugins: [
     react({
-      // babel: {
-      //   plugins: ["babel-plugin-react-compiler"],
-      // },
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
     }),
     tsconfigPaths(),
     visualizer({
